b3185d2ef514a024350ed07271e68c87520c0e61
wei <flashmx29403229@gmail.com>
2024-02-22 13:44:42 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-import { noise_perlin, noise_value } from "./noise.js";
+import { noise_perlin } from "./noise.js";
+
+const setImageDataPixel = (imageData, x, y, r, g, b, a) => {
+  const k = (y * imageData.width + x) * 4;
+  imageData.data[k + 0] = r;
+  imageData.data[k + 1] = g;
+  imageData.data[k + 2] = b;
+  imageData.data[k + 3] = a;
+};
+const loopRect = (x, y, w, h, fun) => {
+  for (let i = x; i < x + w; i++) {
+    for (let j = y; j < y + h; j++) {
+      fun(i, j);
+    }
+  }
+};
+const noiseAPI = (x, y, xN = 0, yN = 0) => {
+  return 0.5 * noise_perlin([xN * x, yN * y], [xN, yN]) + 0.5;
+};
+
 const app = new PIXI.Application({
   resizeTo: window,
 });
@@ -49,94 +68,64 @@ const fragmentSrc = `
           }
       }`;
 
-const canvas = document.createElement("canvas");
 const r = 100;
 const circumference = Math.ceil(r * 2 * Math.PI);
+let rand = [Math.random(), Math.random()];
+let index = 0;
+
+const canvas = document.createElement("canvas");
 canvas.width = circumference;
 canvas.height = r;
-canvas.style.zIndex = 999;
+const ctx = canvas.getContext("2d");
+
+/*canvas.style.zIndex = 999;
 canvas.style.position = "absolute";
 canvas.style.left = "0px";
 canvas.style.top = "0px";
-document.body.appendChild(canvas);
-
-const ctx = canvas.getContext("2d");
-let rand = [Math.random(), Math.random()];
-let index = 0;
-const setImageDataPixel = (imageData, x, y, r, g, b, a) => {
-  const k = (y * imageData.width + x) * 4;
-  imageData.data[k + 0] = r;
-  imageData.data[k + 1] = g;
-  imageData.data[k + 2] = b;
-  imageData.data[k + 3] = a;
-};
-const loopBox = (w, h, fun) => {
-  for (let i = 0; i < w; i++) {
-    for (let j = 0; j < h; j++) {
-      fun(i, j);
-    }
-  }
-};
-const loopRect = (x, y, w, h, fun) => {
-  loopBox(w, h, (rx, ry) => fun(x + rx, y + ry, rx, ry));
-};
+document.body.appendChild(canvas);*/
 
 const renderTexture = PIXI.Texture.from(canvas);
 const uniforms = {
   uSampler: renderTexture,
 };
-
 const shader = PIXI.Shader.from(vertexSrc, fragmentSrc, uniforms);
 
-const triangle = new PIXI.Mesh(geometry, shader);
+const rader = new PIXI.Mesh(geometry, shader);
+rader.position.set(100, 100);
+rader.scale.set(1);
+app.stage.addChild(rader);
 
-triangle.position.set(200, 200);
-triangle.scale.set(1);
+const box = new PIXI.Sprite(renderTexture);
+box.position.set(0, 200);
+app.stage.addChild(box);
 
-app.stage.addChild(triangle);
 const data = new Array(circumference).fill(0).map(() => new Array(r));
-loopRect(0, 0, circumference, r, (x, y, rx, ry) => (data[x][y] = 0));
-const calc = (x, y, offsetX = 0, offsetY = 0) => {
-  const xRate = x / circumference;
-  const yRate = y / r;
+
+const updateTexture = (x, y, w, h, offsetX = 0, offsetY = 0) => {
+  //取得資料並設定到data
   const xN = 20 * 2 * Math.PI;
   const yN = 20;
-  const xx = xN * xRate + xN * offsetX;
-  const yy = yN * yRate + yN * offsetY;
-  return 0.5 * noise_perlin([xx, yy], [xN, yN]) + 0.5;
-};
-const getdata = (x, y, w, h) => {
-  return data.slice(x, x + w).map((el) => el.slice(y, y + h));
-};
-const drawTexture = (x, y, w, h) => {
-  const _data = getdata(x, y, w, h);
+  loopRect(x, y, w, h, (x, y) => (data[x][y] = noiseAPI(x / circumference + offsetX, y / r + offsetY, xN, yN)));
+  //取得data並轉成ImageData及繪製到canvas
+  const _data = data.slice(x, x + w).map((el) => el.slice(y, y + h));
   const imageData = ctx.createImageData(w, h);
-  loopBox(w, h, (x, y) => {
-    setImageDataPixel(imageData, x, y, 0, 255 * _data[x][y], 0, 255);
-  });
+  loopRect(0, 0, w, h, (x, y) => setImageDataPixel(imageData, x, y, 0, 255 * _data[x][y], 0, 255));
   ctx.putImageData(imageData, x, y);
 };
-loopRect(0, 0, circumference, r, (x, y, rx, ry) => {
-  data[x][y] = calc(x,y);
-});
-drawTexture(0, 0, circumference, r);
+
+updateTexture(0, 0, circumference, r);
 app.ticker.add((delta) => {
   const n = Math.floor(0.01 * circumference * delta);
   const _rand = rand;
   if (index + n >= circumference) {
     rand = [Math.random(), Math.random()];
   }
-  loopRect(index, 0, n, r, (x, y, rx, ry) => {
-    const xx = x % circumference;
-    const yy = y % r;
-    data[xx][yy] = calc(xx, yy, ...rand);
-  });
   if (circumference - index >= n) {
-    drawTexture(index, 0, n, r, ..._rand);
+    updateTexture(index, 0, n, r, ..._rand);
   } else {
     const ww = circumference - index;
-    drawTexture(index, 0, ww, r, ..._rand);
-    drawTexture(0, 0, n - ww, r, ...rand);
+    updateTexture(index, 0, ww, r, ..._rand);
+    updateTexture(0, 0, n - ww, r, ...rand);
   }
   index += n;
   index %= circumference;
